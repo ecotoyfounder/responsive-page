2d9258bb744e3541199eaeb4b796c0d0ced32b40
Pavel <M3modelist@gmail.com>
2023-02-17 13:53:24 +0200

    first commit

---
+++ src/app/pages/automatizations/automatizations.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-automatizations',
+  templateUrl: './automatizations.component.html',
+  styleUrls: ['./automatizations.component.css']
+})
+export class AutomatizationsComponent {
+
+}
